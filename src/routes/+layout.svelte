79eddb7b34087b50f8c6354abe8cecc36f9cf44f
Gustavo Santos <gustavofsantos@outlook.com>
2023-06-17 10:17:56 -0300

    configure tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "../app.css";
+</script>
+
+<main class="w-full flex flex-col items-center">
+  <article class="w-full px-2 pt-4 lg:max-w-xl lg:px-0 lg:pt-12">
+    <slot />
+  </article>
+</main>
