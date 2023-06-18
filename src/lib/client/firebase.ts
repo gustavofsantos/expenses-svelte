699b94f65f42a7019ba4fc76aab9005a28d304e5
Gustavo Santos <gustavofsantos@outlook.com>
2023-06-17 21:16:57 -0300

    use svelte static env variables module

---
+++ src/lib/client/firebase.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { PUBLIC_apiKey, PUBLIC_authDomain, PUBLIC_projectId, PUBLIC_storageBucket, PUBLIC_messagingSenderId, PUBLIC_appId, PUBLIC_measurementId } from "$env/static/public"
+
+const firebaseConfig = {
+  apiKey: PUBLIC_apiKey,
+  authDomain: PUBLIC_authDomain,
+  projectId: PUBLIC_projectId,
+  storageBucket: PUBLIC_storageBucket,
+  messagingSenderId: PUBLIC_messagingSenderId,
+  appId: PUBLIC_appId,
+  measurementId: PUBLIC_measurementId
+};
+
+let app
+
+// Initialize Firebase
+if (getApps().length === 0) { 
+  app = initializeApp(firebaseConfig);
+}
+  
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+
