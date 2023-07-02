968a1fcd8911e0ea1aec38b75d9dcec898814296
Gustavo Santos <gustavofsantos@outlook.com>
2023-07-02 19:29:18 -0300

    verify config on startup

---
+++ src/hooks.server.ts
@@ -0,0 +1,19 @@
+import { ENTRIES_FILE_PATH } from '$env/static/private';
+import * as fs from 'node:fs';
+
+function verifyFileOrPathIsWriteable(path: string) {
+	try {
+		fs.accessSync(path, fs.constants.W_OK);
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
+function startApp() {
+	if (!verifyFileOrPathIsWriteable(ENTRIES_FILE_PATH)) {
+		throw new Error('Entries file is not writeable');
+	}
+}
+
+startApp();
